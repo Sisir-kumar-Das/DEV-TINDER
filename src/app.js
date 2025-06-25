4ee95a692b272a292153e28b5420528a6591216b
Sisir <das1234sisir@gmail.com>
2025-06-25 20:51:36 +0530

    Passsword hashing

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const { connectDB } = require("./config/database");
 const app = express();
 const { adminAuth } = require("./middlewares/auth");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 app.use(express.json());
 
@@ -10,13 +13,46 @@ app.use(express.urlencoded({ extended: true }));
 
 //Sign up User and save the data in DB
 app.post("/signup", async (req, res) => {
-  //Creating a new instance of the user Model
-  const user = new User(req.body);
   try {
+    // validation of data
+    validateSignUpData(req);
+    // Ecrypt the password
+    const { firstName, lastName, emailId, password } = req.body;
+    const paswordHash = await bcrypt.hash(password, 10);
+    //Creating a new instance of the user Model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: paswordHash,
+    });
+
     await user.save();
     res.send("User Added Sucessfully.");
   } catch (err) {
-    res.status(500).send("Error Saving the user" + err.message);
+    res.status(500).send("Error: " + err.message);
+  }
+});
+
+//login user
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+    if (!validator.isEmail(emailId)) {
+      throw new Error("Email id is not valid.");
+    }
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Email id is not exist, please sign up.");
+    }
+    const isPaswordValid = await bcrypt.compare(password, user.password);
+    if (isPaswordValid) {
+      res.send("Login Sucessful!");
+    } else {
+      throw new Error("Email ID or password does not exist!");
+    }
+  } catch (err) {
+    res.status(500).send("Error: " + err.message);
   }
 });
 
