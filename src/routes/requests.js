dd1d9e1a2fa7b17313e818347c5c22399588adc5
Sisir <das1234sisir@gmail.com>
2025-07-17 21:17:40 +0530

    connection req api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -3,9 +3,29 @@ const express = require("express");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
-requestRouter.post("/sendConnectiobRequest", userAuth, async (req, res) => {
-  const user = req.user;
-  res.send(user.firstName + "sent the connect request!");
-});
+const ConnectionRequest = require("../models/connectionRequest");
+
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const connectionRequest = new ConnectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await connectionRequest.save();
+      res.json({ message: "Connection request sent succesfully.", data });
+    } catch (err) {
+      res.status(400).send("ERROR: " + err.message);
+    }
+  }
+);
 
 module.exports = requestRouter;
