39832224ddcde60cf3a5f072de03faa7387fbe9d
Sisir <das1234sisir@gmail.com>
2025-07-19 21:07:13 +0530

    validations in connections request

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,9 +1,10 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const requestRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
@@ -13,6 +14,47 @@ requestRouter.post(
       const fromUserId = req.user._id;
       const toUserId = req.params.toUserId;
       const status = req.params.status;
+
+      const allowedStatus = ["ignored", "intrested"];
+
+      if (!allowedStatus.includes(status)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid status type: " + status });
+      }
+
+      if (fromUserId.toString() === toUserId) {
+        return res
+          .status(400)
+          .json({ message: " From and Receiver user is same." });
+      }
+
+      if (!mongoose.Types.ObjectId.isValid(toUserId)) {
+        return res
+          .status(400)
+          .json({ message: "Invalid toUserId: " + toUserId });
+      }
+
+      const toUser = await User.findById(toUserId);
+
+      if (!toUser) {
+        return res.status(400).json({ message: "User Id doesn't exist." });
+      }
+
+      // check if there is existing connectionRequest
+
+      const existingConnetctionREquest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+
+      if (existingConnetctionREquest) {
+        return res
+          .status(400)
+          .json({ message: "Connection request already exist!" });
+      }
 
       const connectionRequest = new ConnectionRequest({
         fromUserId,
