2e9c315d2cdb4592b83c1d417a0be1707e368913
Sisir <das1234sisir@gmail.com>
2025-07-25 20:39:46 +0530

    connection requests api

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,13 +14,36 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequest.find({
       toUserId: loggedInUser._id,
       status: "intrested",
-    });
+    }).populate("fromUserId", [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "age",
+      "about",
+      "skills",
+    ]);
     if (!connectionRequests.length) {
       return res.status(400).json({ message: "No request found" });
     }
     res
       .status(200)
       .json({ message: "Data fetched sucessfully", data: connectionRequests });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "intrested" },
+        { toUserId: loggedInUser._id, status: "intrested" },
+      ],
+    });
+
+    res.status(200).json({ data: connectionRequests });
   } catch (err) {
     res.status(400).send("Error: " + err.message);
   }
