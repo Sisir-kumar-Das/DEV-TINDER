aa4073af6a6d64023ba5c0e8d3c7490da1340354
Sisir <das1234sisir@gmail.com>
2026-05-27 17:28:28 +0530

    remove checks

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -16,9 +16,9 @@ profileRouter.get("/profile/view", userAuth, async (req, res) => {
 
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   try {
-    if (!validateEditProfileData(req)) {
-      throw new Error("Invalid Edit Request!");
-    }
+    // if (!validateEditProfileData(req)) {
+    //   throw new Error("Invalid Edit Request!");
+    // }
 
     const loggedInUser = req.user;
     Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
